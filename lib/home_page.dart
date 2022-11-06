482d7461609bbc8fadaec02314370118936e828a
Pasindu Shavinda <pasindu.shv@gmail.com>
2022-11-06 22:57:18 +0530

    Firebase Database Configurations Done

---
+++ lib/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Main extends StatefulWidget {
+  const Main({Key? key}) : super(key: key);
+
+  @override
+  State<Main> createState() => _MainState();
+}
+
+class _MainState extends State<Main> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
