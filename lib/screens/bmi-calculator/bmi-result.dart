41604b264795b31b9bb1ea45277c9932e45abdb0
Pasindu Shavinda <pasindu.shv@gmail.com>
2022-11-08 20:14:30 +0530

    BMI calculator partially completed

---
+++ lib/screens/bmi-calculator/bmi-result.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BMIResult extends StatefulWidget {
+  const BMIResult({Key? key}) : super(key: key);
+
+  @override
+  State<BMIResult> createState() => _BMIResultState();
+}
+
+class _BMIResultState extends State<BMIResult> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
